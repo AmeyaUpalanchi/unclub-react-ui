51be98c4e7874ec017a7ceeeb978713f8862ef25
AmeyaUpalanchi <ameya.upalanchi18@vit.edu>
2021-08-03 21:58:41 +0530

    docs(theme): storybook docs theme

---
+++ .storybook/customTheme.js
@@ -0,0 +1,8 @@
+import { create } from '@storybook/theming';
+import './styles.css'
+
+export default create({
+  base: 'light',
+  brandTitle: 'Unclub React UI',
+  brandUrl: 'https://ameyaupalanchi.github.io/unclub-react-ui/',
+});
