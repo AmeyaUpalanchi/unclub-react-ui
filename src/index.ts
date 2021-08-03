89624a12af27d182986a4bfdd986dbf4deb882c4
AmeyaUpalanchi <ameya.upalanchi18@vit.edu>
2021-08-03 20:35:34 +0530

    chore(entryfile): add entryfile

---
+++ src/index.ts
@@ -0,0 +1,23 @@
+/**
+ * *Atoms 
+*/
+
+export {default as Avatar} from './atoms/avatar/Avatar';
+export {default as Badge} from './atoms/badge/Badge';
+export {default as Button} from './atoms/button/Button';
+export {default as Input} from './atoms/input/Input';
+export {default as Textarea} from './atoms/textarea/Textarea';
+
+/**
+ * * Molecules 
+*/
+
+export {default as Alert} from './molecules/alert/Alert';
+export {default as Backdrop} from './molecules/backdrop/Backdrop';
+
+/**
+ * * Organisms 
+*/
+
+export {default as Card} from './organisms/card/Card';
+export {default as CardBody} from './organisms/card/CardBody';
